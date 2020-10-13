649d152b35898ca2a8f33ab8fe3b33dbf1c1df1b
SIM <sintim.misa@gmail.com>
2020-10-13 00:24:09 +0000

    FEA: setup cors,morgan and bodyparser

--- database/config.js
+++ database/config.js
@@ -16,5 +16,4 @@ const connectDB= async()=>{
         }
     };
 
-
-    module.exports= connectDB;
+ module.exports= connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express =require('express');
+const cors =require('cors');
+const logger=require('morgan');
+
 require('dotenv').config()//For loading environment variable
 const connectDB =require('./database/config');//for connecting Db
 
 const app=express();
 
 const PORT=process.env.PORT;
+
+/**
+ * middlewares(methods/functions/operations that are called BETWEEN processing a Request 
+ * and sending a Response )
+ * @cors() to handle http request from different origin(domain,protocol,port)
+ * @Morgan to handle request logging
+ * @bodyparser { currently express.json in v4.16^} parse incoming request as json obj
+ */
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+
 
 
 //Datebase connection
